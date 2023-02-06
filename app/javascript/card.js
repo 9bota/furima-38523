22a84f390397d0550ddebb48937c9dcec6e70e22
9bota <118107164+9bota@users.noreply.github.com>
2023-02-06 20:45:10 +0900

    Update card.js

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);// PAY.JPテスト公開鍵
+  const payjp = Payjp(process.env.PAYJP_PUBLIC_KEY);// PAY.JPテスト公開鍵
   const form = document.getElementById("charge-form");
   const elements = payjp.elements();
   const numberElement = elements.create('cardNumber');
